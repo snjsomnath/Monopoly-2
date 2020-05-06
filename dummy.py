ca9f1fd42592fcd54830e10bd17f4ba99651b66b
snjsomnath <53001297+snjsomnath@users.noreply.github.com>
2020-05-06 07:40:17 +0200

    dashboard updated

--- dummy.py
+++ dummy.py
@@ -4,17 +4,25 @@ rankCSV = "rank.csv"
 players = ["Sanjay", "thamarmo", "Abhishek", "Shishtaouk", "NipPincher", "Diraj", "SugaDaddy", "Spartan"]
 data_r = pd.read_csv(rankCSV)
 winRatio = []
-wins
-
+wins =[]
+played = []
+losses = []
 
 for i in players:
     gamesPlayed= data_r[i].value_counts().sum()
+    played.append(gamesPlayed)
     if 1 in data_r[i].unique():
         win = data_r[i].value_counts()[1]
+        wins.append(win)
         wr = ((win/gamesPlayed*100).round(2))
         winRatio.append(wr)
     else:
         winRatio.append(0)
-print(winRatio)
+        wins.append(0)
+for i in range(len(played)):
+    losses.append(played[i]-wins[i])
+winLoss = pd.DataFrame(list(zip(players, wins, played,losses,winRatio)), 
+               columns =['Names', 'Wins', 'Played', 'Lost', 'winRatio'])
+winLoss.to_csv(r'winLoss.csv', index = False) 
 
 
--- test.py
+++ test.py
@@ -131,5 +131,27 @@ mergedData_rank = pd.concat([data_r, ocrRank], join = "outer")
 mergedData_rank.to_csv(rankCSV, index=False)
 
 #Calculate wins and win-ratio
-data_r.heads
-print("Run complete")
+players = ["Sanjay", "thamarmo", "Abhishek", "Shishtaouk", "NipPincher", "Diraj", "SugaDaddy", "Spartan"]
+data_r = pd.read_csv(rankCSV)
+winRatio = []
+wins =[]
+played = []
+losses = []
+
+for i in players:
+    gamesPlayed= data_r[i].value_counts().sum()
+    played.append(gamesPlayed)
+    if 1 in data_r[i].unique():
+        win = data_r[i].value_counts()[1]
+        wins.append(win)
+        wr = ((win/gamesPlayed*100).round(2))
+        winRatio.append(wr)
+    else:
+        winRatio.append(0)
+        wins.append(0)
+for i in range(len(played)):
+    losses.append(played[i]-wins[i])
+winLoss = pd.DataFrame(list(zip(players, wins, played,losses,winRatio)), 
+               columns =['Names', 'Wins', 'Played', 'Lost', 'winRatio'])
+winLoss.to_csv(r'winLoss.csv', index = False) 
+
